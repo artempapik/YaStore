567cf6617b18d33fc38088cc5b13960989522004
artemy <artemy@SW-DEV12>
2019-06-10 10:56:12 +0300

    nothing

--- YaStore/ClientApp/src/app/app.server.module.ts
+++ YaStore/ClientApp/src/app/app.server.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { ServerModule } from '@angular/platform-server';
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 import { AppModule } from './app.module';
 
 @NgModule({
